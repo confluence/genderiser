64f8dd13f6338587f1e89b0bbcbc4a99459ea3c3
Adrianna Pińska <adrianna.pinska@gmail.com>
2014-07-25 14:16:38 +0200

    basic api should be working; needs proper testing

--- genderiser.py
+++ genderiser.py
@@ -3,7 +3,6 @@
 import re
 import os
 import sys
-import glob
 import ConfigParser
 import StringIO
 import argparse
@@ -35,9 +34,8 @@ class Genderiser(object):
                 else: # generic word
                     self.substitutions['%s_%s' % (surname, key)] = value
 
-    def replace(self, *files, **kwargs):
-        output_dir = kwargs.get('output_dir', 'output')
-        if not os.path.exists(output_dir):
+    def replace(self, output_dir, *files):
+        if output_dir is not None and not os.path.exists(output_dir):
             os.mkdir(output_dir)
 
         variable_regex = self.cp.get('main', 'variable_regex')
@@ -57,26 +55,34 @@ class Genderiser(object):
             else:
                 return 'UNKNOWN'
 
-        for filename in files:
+        for i, filename in enumerate(files):
             with open(filename, 'r') as infile:
-                outfilename = os.path.join(output_dir, os.path.basename(filename))
-                with open(outfilename, 'w') as outfile:
-                    for line in infile:
-                        line = VAR.sub(var_sub, line)
-                        outfile.write(line)
 
-    def subs_as_text(self):
-        pass
+                if output_dir is not None:
+                    outfilename = os.path.join(output_dir, os.path.basename(filename))
+                    with open(outfilename, 'w') as outfile:
+                        for line in infile:
+                            outfile.write(VAR.sub(var_sub, line))
+
+                else:
+                    for line in infile:
+                        print VAR.sub(var_sub, line)
+
+                    if i + 1 < len(files):
+                        print "----"
+
+    def __str__(self):
+        return ",".join("%s:%s" % (k, v) for (k, v) in self.substitutions.iteritems())
          
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Replace placeholder variables with gendered words in text files")
-    parser.add_argument("dir", help="Directory of files to process")
-    parser.add_argument("-d", "--config-dir", help="Directory with your custom configuration files. By default the directory with files to be processed will be used.")
+    parser.add_argument("files", help="Files to process", nargs="?")
+    parser.add_argument("-d", "--config-dir", default=".", help="Directory with your custom configuration files. By default the working directory will be used.")
     parser.add_argument("-o", "--output-dir", default="output", help="Directory to which modified files will be written.")
     parser.add_argument("-c", "--config", help="String containing custom config to be used. Replaces the custom config directory.")
     parser.add_argument("-s", "--subs-only", help="Don't process any files; just output a list of substitutions", action="store_true")
-    parser.add_argument("-v", "--verbose", help="Turn on verbose output", action="count")
+    parser.add_argument("-p", "--preview", help="Suppress writing of output files and print the modified file contents.", action="store_true")
 
     args = parser.parse_args()
 
@@ -86,15 +92,17 @@ if __name__ == "__main__":
         gen.read_config(text=args.config)
     elif args.config_dir:
         gen.read_config('config', os.path.join(args.config_dir,'config'))
-    elif args.dir:
-        gen.read_config('config', os.path.join(args.dir,'config'))
-    elif not args.subs_only:
-        print "No project directory specified. Exiting."
+    else:
+        print "No config specified. Exiting."
         sys.exit(1)
 
     if args.subs_only:
-        print gen.subs_as_text()
-    elif args.dir:
-        files = glob.glob(os.path.join(args.dir, '*.txt'))
-        gen.replace(output_dir, *files, **{'output_dir': args.output_dir})
+        print gen
+    elif args.files:
+        output_dir = args.output_dir if not args.preview else None
+        gen.replace(output_dir, *args.files)
+    else:
+        print "No files specified. Exiting."
+        sys.exit(1)
+        
     
