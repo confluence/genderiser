368e12cbbd79fbbeeb5ffc7836ad0488025c3052
Adrianna Pińska <adrianna.pinska@gmail.com>
2014-07-26 13:25:37 +0200

    a lot of refactoring

--- genderiser.py
+++ genderiser.py
@@ -2,110 +2,135 @@
 
 import re
 import os
+import glob
 import sys
 import ConfigParser
 import StringIO
 import argparse
 
+class GenderiserError(Exception):
+    pass
+
 class Genderiser(object):
 
     def __init__(self):
         self.cp = ConfigParser.SafeConfigParser()
-        self.substitutions = {}
+        self.subs = {}
+        self.missing = set()
 
-    def read_config(self, *files, **kwargs):
-        if 'text' in kwargs:
-            self.cp.readfp(StringIO.StringIO(kwargs['text']))
-        else:
-            self.cp.read(files)
+    def read_config(self, project_dir=None, config_text=None):
+        files = ["genderiser.cfg"]
+        if project_dir is not None:
+            files.extend(glob.glob(os.path.join(project_dir, "*.cfg")))
+        self.cp.read(files)
         
-        for surname, gender in self.cp.items('characters'):
-            if gender not in self.cp.options('genders'):
-                raise ValueError('%r is not listed in the genders configuration section.' % gender)
+        if config_text is not None:
+            self.cp.readfp(StringIO.StringIO(config_text))
+        
+        for surname, gender in self.cp.items("characters"):
+            if gender not in self.cp.options("genders"):
+                raise ValueError("%r is not listed in the genders configuration section." % gender)
 
             if gender not in self.cp.sections():
-                raise ValueError('No configuration section found for gender %r.' % gender)
+                raise ValueError("No configuration section found for gender %r." % gender)
 
             for key, value in self.cp.items(gender):
-                if '_' in key and key.startswith(surname): # special variable for this character
-                    self.substitutions[key] = value
-                elif '_' in key: # special variable for a different character
+                if "_" in key and key.startswith(surname): # special variable for this character
+                    self.subs[key] = value
+                elif "_" in key: # special variable for a different character
                     continue
                 else: # generic word
-                    self.substitutions['%s_%s' % (surname, key)] = value
+                    self.subs["%s_%s" % (surname, key)] = value
 
-    def replace(self, output_dir, *files):
-        if output_dir is not None and not os.path.exists(output_dir):
-            os.mkdir(output_dir)
+    def replace(self, project_dir, output_dir=None, preview=False):
+        files = []
 
-        variable_regex = self.cp.get('main', 'variable_regex')
-        key_error_mode = self.cp.get('main', 'key_error_mode')
+        for filename in self.cp.get("files", "files").split(","):
+            files.append(filename.strip())
+
+        if self.cp.has_option("files", "glob"):
+            files.extend(glob.glob(self.cp.get("files", "glob")))
+
+        if output_dir is None:
+            output_dir = os.path.join(project_dir, "output")
+        
+        if not preview and os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        variable_regex = self.cp.get("main", "variable_regex")
+        key_error_mode = self.cp.get("main", "key_error_mode")
 
         VAR = re.compile(variable_regex)
 
         def var_sub(m):
             surname, word = m.groups()
-            sub = self.substitutions.get('%s_%s' % (surname.lower(), word.lower()))
-            if sub:
-                if surname != surname.lower():
-                    return sub.capitalize()
-                return sub
-            elif key_error_mode == 'quiet':
+            key = "%s_%s" % (surname.lower(), word.lower())
+            replacement = self.subs.get(key)
+            if replacement:
+                if surname == surname.capitalize():
+                    return replacement.capitalize()
+                return replacement
+            elif key_error_mode == "quiet":
                 return word
             else:
-                return 'UNKNOWN'
+                return "UNKNOWN"
 
         for i, filename in enumerate(files):
-            with open(filename, 'r') as infile:
+            with open(os.path.join(project_dir, filename), "r") as infile:
+                text = infile.read()
+                text = VAR.sub(var_sub, text)
 
-                if output_dir is not None:
-                    outfilename = os.path.join(output_dir, os.path.basename(filename))
-                    with open(outfilename, 'w') as outfile:
-                        for line in infile:
-                            outfile.write(VAR.sub(var_sub, line))
+                # print a preview to stdout
+                if preview is not None:
+                    preview.write(text)
 
-                else:
-                    for line in infile:
-                        yield VAR.sub(var_sub, line)
+                # otherwise try to write to a file
+                elif output_dir is not None:
+                    outfilename = os.path.join(output_dir, filename)
 
-                    if i + 1 < len(files):
-                        yield "----"
+                    new_file_dir = os.path.dirname(filename)
+                    if not os.path.exists(new_file_dir):
+                        os.makedirs(new_file_dir)
 
-    def __str__(self):
-        return ",".join("%s:%s" % (k, v) for (k, v) in self.substitutions.iteritems())
-         
+                    with open(outfilename, "w") as outfile:
+                        outfile.write(text)
+
+    def subs_as_text(self):
+        return ",".join("%s:%s" % (k, v) for (k, v) in self.subs.iteritems())
+
+    def missing_as_text(self):
+        return ",".join(m for m in self.missing)
+
+    def from_args(self, args):
+        self.read_config(args.dir, args.config)
+
+        if args.substitutions:
+            print self.subs_as_text()
+
+        elif args.missing:
+            print self.missing_as_text()
+
+        elif args.dir:
+            output_dir = args.output_dir if not args.no_output else None
+            preview = sys.stdout if args.preview else None
+            self.replace(args.dir, output_dir, preview)
+        else:
+            raise GenderiserError("No project specified and nothing to do.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Replace placeholder variables with gendered words in text files")
-    parser.add_argument("files", help="Files to process", nargs="?")
-    parser.add_argument("-d", "--config-dir", default=".", help="Directory with your custom configuration files. By default the working directory will be used.")
-    parser.add_argument("-o", "--output-dir", default="output", help="Directory to which modified files will be written.")
-    parser.add_argument("-c", "--config", help="String containing custom config to be used. Replaces the custom config directory.")
-    parser.add_argument("-s", "--subs-only", help="Don't process any files; just output a list of substitutions", action="store_true")
-    parser.add_argument("-p", "--preview", help="Suppress writing of output files and print the modified file contents.", action="store_true")
+    parser.add_argument("dir", help="Project directory to process", nargs="?", default=None)
+    parser.add_argument("-o", "--output-dir", default="output", help="Directory to which modified files will be written. By default a directory named 'output' will be created in the project directory.")
+    parser.add_argument("-c", "--config", help="String containing custom config to be used. May be used in addition to or instead of a project directory.", default=None)
+    parser.add_argument("-s", "--substitutions", help="Suppress all other output and print a list of substitutions.", action="store_true")
+    parser.add_argument("-p", "--preview", help="Suppress all other output and print the modified file contents to standard output.", action="store_true")
+    parser.add_argument("-m", "--missing", help="Suppress all other output and print a list of variables for which no replacements could be found.", action="store_true")
 
     args = parser.parse_args()
 
     gen = Genderiser()
+    gen.from_args(args)
 
-    if args.config:
-        gen.read_config(text=args.config)
-    elif args.config_dir:
-        gen.read_config('config', os.path.join(args.config_dir,'config'))
-    else:
-        print "No config specified. Exiting."
-        sys.exit(1)
 
-    if args.subs_only:
-        print gen
-    elif args.files:
-        if args.preview:
-            for line in gen.replace(None, *args.files):
-                print line
-        else:
-            gen.replace(args.output_dir, *args.files)
-    else:
-        print "No files specified. Exiting."
-        sys.exit(1)
         
     
--- test.py
+++ test.py
@@ -1,40 +1,43 @@
 import unittest
 import os
+import StringIO
 from genderiser import Genderiser
 
 class TestGenderiser(unittest.TestCase):
     def test_config(self):
         expected_substitutions = {
-            'jones_name': 'Mary',
-            'jones_person': 'woman',
-            'jones_sibling': 'sister',
-            'jones_their': 'her',
-            'jones_theirs': 'hers',
-            'jones_them': 'her',
-            'jones_themselves': 'herself',
-            'jones_they': 'she',
-            'smith_name': 'John',
-            'smith_person': 'man',
-            'smith_sibling': 'brother',
-            'smith_their': 'his',
-            'smith_theirs': 'his',
-            'smith_them': 'him',
-            'smith_themselves': 'himself',
-            'smith_they': 'he',
+            "jones_name": "Mary",
+            "jones_person": "woman",
+            "jones_sibling": "sister",
+            "jones_their": "her",
+            "jones_theirs": "hers",
+            "jones_them": "her",
+            "jones_themselves": "herself",
+            "jones_they": "she",
+            "smith_name": "John",
+            "smith_person": "man",
+            "smith_sibling": "brother",
+            "smith_their": "his",
+            "smith_theirs": "his",
+            "smith_them": "him",
+            "smith_themselves": "himself",
+            "smith_they": "he",
         }
 
         g = Genderiser()
-        g.read_config('config', os.path.join('example','config'))
-        self.assertEqual(g.substitutions, expected_substitutions)
+        g.read_config("example")
+        self.assertEqual(g.subs, expected_substitutions)
 
     def test_parsing(self):
         expected_output = "You know a man called John Smith. He has a sister called Mary Jones."
         
         g = Genderiser()
-        g.read_config('config', os.path.join('example','config'))
-        output = "".join(line for line in g.replace(None, os.path.join('example','Alice.txt'))).strip()
-        self.assertEqual(output, expected_output)
+        g.read_config("example")
+
+        output = StringIO.StringIO()
+        g.replace("example", None, output)
+        self.assertEqual(output.getvalue().strip(), expected_output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
